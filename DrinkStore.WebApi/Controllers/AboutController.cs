9ca41da7192b8ed0ebd49286a3aabcb2d8a92102
Robert Goheen <goheen@gmail.com>
2016-12-27 13:57:16 +0000

    Default about controller

---
+++ DrinkStore.WebApi/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Web.Http;
+
+namespace DrinkStore.WebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class AboutController : ApiController
+    {
+        [Route("")]
+        [Route("about")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new {Message = $"Web api version: {Assembly.GetExecutingAssembly().GetName().Version}"});
+        }
+    }
+}
